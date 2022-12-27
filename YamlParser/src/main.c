b8ab39119654372afdb8cffe17ff91a8bfaee528
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2022-12-27 00:10:35 -0600

    Delete YamlParser directory

--- YamlParser/src/main.c
+++
@@ -1,7 +0,0 @@
-#include "parser.h" 
-
-int main(int args, char *argv[])
-{
-	open_and_parse_yaml("yaml_tests/test1.yaml");
-	return 0;
-}
--- YamlParser/src/test.c
+++
@@ -1,8 +0,0 @@
-#include "parser.h" 
-
-int main(int args, char *argv[])
-{
-	_yaml_os_data yod = open_and_parse_yaml("yaml_tests/test1.yaml");
-	
-	return 0;
-}
