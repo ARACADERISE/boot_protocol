9a103ebfd7856f6ecf9e2d373ed6c9aaa794af53
Moca <36897041+MocaCDev@users.noreply.github.com>
2022-12-30 05:18:17 -0600

    Update gdt.asm

--- boot/gdt.asm
+++ boot/gdt.asm
@@ -1,4 +1,4 @@
-g_GDT:      ; NULL descriptor
+working_bit32_bit16_GDT:      ; NULL descriptor
             .null_desc          dq 0x0
 
             ; 32-bit code segment
@@ -33,13 +33,37 @@ g_GDT:      ; NULL descriptor
             .data16_gran        db 00001111b                ; granularity (1b pages, 16-bit pmode) + limit (bits 16-19)
             .data16_base_high   db 0                        ; base high
 
-def_GDTDesc:
-    dw def_GDTDesc - g_GDT - 1
-    dd g_GDT
+working_bit32_bit16_GDTDesc:
+    dw working_bit32_bit16_GDTDesc - working_bit32_bit16_GDT - 1
+    dd working_bit32_bit16_GDT
 
-g_GDT32_16_address     equ 0xA000
-g_GDT32_16_desc_addr   equ 0xAA00
-g_GDT_status:          equ 0xC000
+working_bit32_GDT:
+            .null_desc1          dq 0x0
+
+            ; 32-bit code segment
+            .code32_limit1       dw 0xFFFF                   ; limit (bits 0-15) = 0xFFFFF for full 32-bit range
+            .code32_base1        dw 0                        ; base (bits 0-15) = 0x0
+            .code32_base21       db 0                        ; base (bits 16-23)
+            .code32_acces1       db 10011010b                ; access (present, ring 0, code segment, executable, direction 0, readable)
+            .code32_gran1        db 11001111b                ; granularity (4k pages, 32-bit pmode) + limit (bits 16-19)
+            .code32_base_high1   db 0                        ; base high
+
+            ; 32-bit data segment
+            .data32_limit1       dw 0FFFFh                   ; limit (bits 0-15) = 0xFFFFF for full 32-bit range
+            .data32_base1        dw 0                        ; base (bits 0-15) = 0x0
+            .data32_base21       db 0                        ; base (bits 16-23)
+            .data32_access1      db 10010010b                ; access (present, ring 0, data segment, executable, direction 0, writable)
+            .data32_gran1        db 11001111b                ; granularity (4k pages, 32-bit pmode) + limit (bits 16-19)
+            .data32_base_high1   db 0                       ; base high
+
+working_bit32_GDTDesc:
+    dw working_bit32_GDTDesc - working_bit32_GDT - 1
+    dd working_bit32_GDT
+
+
+g_GDT_address       equ 0xA000
+g_GDT_desc_addr     equ 0xAA00
+g_GDT_status:       equ 0xC000
 
 g_GDT32_16:      
             ; NULL descriptor
@@ -80,23 +104,3 @@ g_GDT32_16:
 g_GDTDesc:
     .size dw 0x0
     .addr dd 0x0
-
-;g_GDT32:      
-;            ; NULL descriptor
-;            dq 0
-;
-;            ; 32-bit code segment
-;            dw 0                        ; limit (bits 0-15) = 0xFFFFF for full 32-bit range
-;            dw 0                        ; base (bits 0-15) = 0x0
-;            db 0                        ; base (bits 16-23)
-;            db 0                        ; access (present, ring 0, code segment, executable, direction 0, readable)
-;            db 0                        ; granularity (4k pages, 32-bit pmode) + limit (bits 16-19)
-;            db 0                        ; base high
-;
-;            ; 32-bit data segment
-;            dw 0                        ; limit (bits 0-15) = 0xFFFFF for full 32-bit range
-;            dw 0                        ; base (bits 0-15) = 0x0
-;            db 0                        ; base (bits 16-23)
-;            db 0                        ; access (present, ring 0, data segment, executable, direction 0, writable)
-;            db 0                        ; granularity (4k pages, 32-bit pmode) + limit (bits 16-19)
-;            db 0                        ; base high
