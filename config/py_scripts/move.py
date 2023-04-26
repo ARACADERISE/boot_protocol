6cde2e002a2688f46db4d0b01dcbee7d291d3f18
Moca <36897041+MocaCDev@users.noreply.github.com>
2023-04-25 23:11:10 -0500

    Update move.py

--- config/py_scripts/move.py
+++ config/py_scripts/move.py
@@ -2,8 +2,9 @@
 # This script moves all the scripts(located in `scripts`) to `/usr/bin/`
 import subprocess, os, sys
 
-all_scripts = ['FAMP_fdi']
-all_binaries = ['format_disk_image.o']
+all_tool_scripts = ['FAMP_fdi']
+all_tool_binaries = ['format_disk_image.o']
+all_fimg_binaries = [i for i in os.listdir('bin') if '.fimg' in i]
 usr_bin = '/usr/bin/'
 scripts_path = f'{os.getcwd()}/config/scripts/'
 formats_path = f'{os.getcwd()}/config/formats'
@@ -18,29 +19,36 @@ if not os.path.exists('/usr/lib/FAMP_disk_images'):
     subprocess.run('sudo mkdir /usr/lib/FAMP_disk_images', shell=True, cwd=os.getcwd())
     print('Created /usr/lib/FAMP_disk_images')
 
+if len(sys.argv) > 1 and sys.argv[1] == 'move_disk_image':
+    for i in all_fimg_binaries:
+        subprocess.run(f'sudo mv {os.getcwd()}/bin/{i} /usr/lib/FAMP_disk_images', shell=True, cwd=os.getcwd())
+    
+    print(f'Moved {len(all_fimg_binaries)} disk image{"s" if len(all_fimg_binaries) > 1 else ""} into /usr/lib/FAMP_disk_images')
+    sys.exit(0)
+
 if len(os.listdir('/usr/lib/FAMP_tools_bin')) == 0:
     print('Moving ', end = '')
-    for i in all_binaries:
-        if not i == all_binaries[len(all_binaries)-1]:print(i + ', ', end = '')
+    for i in all_tool_binaries:
+        if not i == all_tool_binaries[len(all_tool_binaries)-1]:print(i + ', ', end = '')
         else:print(i, end = '')
     print(' into /usr/lib/FAMP_tools_bin')
-    for i in range(len(all_binaries)):
-        subprocess.run(f'sudo mv {os.getcwd()}/tools_bin/{all_binaries[i]} /usr/lib/FAMP_tools_bin', shell=True, cwd=os.getcwd())
+    for i in range(len(all_tool_binaries)):
+        subprocess.run(f'sudo mv {os.getcwd()}/tools_bin/{all_tool_binaries[i]} /usr/lib/FAMP_tools_bin', shell=True, cwd=os.getcwd())
 
 # Make sure all scripts are located in `/usr/bin`
 # If not, move the script there
-for i in range(len(all_scripts)):
-    if not os.path.isfile(f'{usr_bin}{all_scripts[i]}'):
-        print(f'Moving {all_scripts[i]} into {usr_bin}')
-        if os.path.isfile(f'{scripts_path}{all_scripts[i]}'):
-            subprocess.run(f'sudo mv {scripts_path}{all_scripts[i]} /usr/bin', shell=True, cwd=os.getcwd())
+for i in range(len(all_tool_scripts)):
+    if not os.path.isfile(f'{usr_bin}{all_tool_scripts[i]}'):
+        print(f'Moving {all_tool_scripts[i]} into {usr_bin}')
+        if os.path.isfile(f'{scripts_path}{all_tool_scripts[i]}'):
+            subprocess.run(f'sudo mv {scripts_path}{all_tool_scripts[i]} /usr/bin', shell=True, cwd=os.getcwd())
             moved_something = True
         else:
             file_contents = open(script_formats[i], 'r').read()
-            with open(f'{scripts_path}{all_scripts[i]}', 'w') as file:
+            with open(f'{scripts_path}{all_tool_scripts[i]}', 'w') as file:
                 file.write(file_contents)
                 file.close()
-            subprocess.run(f'sudo mv {scripts_path}{all_scripts[i]} /usr/bin', shell=True, cwd=os.getcwd())
+            subprocess.run(f'sudo mv {scripts_path}{all_tool_scripts[i]} /usr/bin', shell=True, cwd=os.getcwd())
             moved_something = True
                 
 
@@ -48,10 +56,10 @@ for i in range(len(all_scripts)):
 # In that case, prompt to the user that setting up FAMP tools was indeed successful
 if moved_something:
     # Make sure all the scripts are runnable
-    for i in range(len(all_scripts)):
-        subprocess.run(f'sudo chmod +x {usr_bin}{all_scripts[i]}', shell=True, cwd=os.getcwd())
+    for i in range(len(all_tool_scripts)):
+        subprocess.run(f'sudo chmod +x {usr_bin}{all_tool_scripts[i]}', shell=True, cwd=os.getcwd())
     
     # Make sure all the binaries are runnable
-    for i in range(len(all_binaries)):
-        subprocess.run(f'sudo chmod +x /usr/lib/FAMP_tools_bin/{all_binaries[i]}', shell=True, cwd=os.getcwd())
+    for i in range(len(all_tool_binaries)):
+        subprocess.run(f'sudo chmod +x /usr/lib/FAMP_tools_bin/{all_tool_binaries[i]}', shell=True, cwd=os.getcwd())
     print('Setup for FAMP tools successful!')
