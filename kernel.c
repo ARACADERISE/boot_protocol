889b9c0ea859b5392d3f40a0255286b923d576b3
Moca <36897041+MocaCDev@users.noreply.github.com>
2023-04-25 23:16:09 -0500

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -3,12 +3,23 @@
 
 void starting_point main()
 {
-    clear_screen(black);
+    clear_screen(blue);
+    init_cursor_pos();
+    
+    uint8 key = 1;
+
+    while(1)
+    {
+        key = get_key();
+        if(key == 0x01) break;
+        if(key == 0) continue;
+        put_char(key);
+    }
 
     /* The first byte in the program `mbr_partition_table.c`(binary `mbr_partition_tabe.bin`)
-     * should have the value 0xF3.
+     * should have the value 0x66.
      */
-    if(instant_free_addresses[0][0] == 0xF3)
+    if(instant_free_addresses[0][0] == 0x66)
         print("\n\n\n\tHello, @r World@re\n\t\t*evil laugh*");
 
     /* Halt. */
